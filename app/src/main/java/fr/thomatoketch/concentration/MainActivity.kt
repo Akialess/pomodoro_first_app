52c4a91454ad814f9f46e64e04a0980957860365
Thomas Raymond <thomas.raymond@efrei.net>
2023-07-18 19:24:52 +0200

    ajout des fichier de style des boutons

--- app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
+++ app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.CountDownTimer
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.view.View
 
 import android.animation.ObjectAnimator
 import android.widget.ImageView
@@ -14,15 +15,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var countdownTextView: TextView
     private lateinit var countDownTimer: CountDownTimer
     private lateinit var startButton: Button
+    private lateinit var pauseButton: Button
+    private lateinit var giveUpButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         countdownTextView = findViewById(R.id.décompte)
-        startButton = findViewById(R.id.Boutton_rebours)
+        startButton = findViewById<Button>(R.id.Boutton_rebours)
+        pauseButton= findViewById<Button>(R.id.bouton_pause)
+        giveUpButton = findViewById<Button>(R.id.bouton_abandon)
 
         startButton.setOnClickListener {
+            startButton.visibility = View.GONE
+            pauseButton.visibility = View.VISIBLE
+            giveUpButton.visibility = View.VISIBLE
             startCountdown(3600) // Démarrer le compte à rebours de 10 secondes
         }
     }
