fd54721e1d645311ad1285e054b7409d9b5a7341
Akialess <alessandro.giansante03@gmail.com>
2023-07-21 16:24:09 +0200

    Restructure code and files to accommodate multiple fragments + correct variable names and layout

--- app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
+++ app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
@@ -2,20 +2,16 @@ package fr.thomatoketch.concentration
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.os.PersistableBundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import fr.thomatoketch.concentration.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //injecter le fragment dans notre boite (fragment_container)
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.replace(R.id.fragment_container, HomeFragment(this)) //modifier le dernier param pour afficher une autre page
         transaction.addToBackStack(null)
         transaction.commit()
     }
