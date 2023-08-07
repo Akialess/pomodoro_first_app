6bf6ea98acce0796a9a2825c88cc5326e9ee2469
Akialess <akialess@hotmail.com>
2023-08-07 17:37:08 +0200

    Add design of task page

--- app/src/main/java/fr/thomatoketch/concentration/fragments/TaskFragment.kt
+++ app/src/main/java/fr/thomatoketch/concentration/fragments/TaskFragment.kt
@@ -1,7 +1,6 @@
 package fr.thomatoketch.concentration.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,19 +9,18 @@ import androidx.recyclerview.widget.RecyclerView
 import fr.thomatoketch.concentration.FolderRepository.Singleton.folderList
 import fr.thomatoketch.concentration.MainActivity
 import fr.thomatoketch.concentration.R
-import fr.thomatoketch.concentration.TaskFolderModel
 import fr.thomatoketch.concentration.adapter.TaskFolderAdapter
 
 class TaskFragment(private val context: MainActivity) : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_task, container, false)
+        val view = inflater?.inflate(R.layout.fragment_folder, container, false)
 
         //ne pas oublier d'ajouter l'icone
 
         //afficher les dossiers en mode vertical
         val verticalRecyclerView = view?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
 
-        verticalRecyclerView?.adapter = TaskFolderAdapter(context, folderList, R.layout.item_task) //folderlist vient du singleton
+        verticalRecyclerView?.adapter = TaskFolderAdapter(context, folderList, R.layout.item_folder) //folderlist vient du singleton
 
         return view
     }
