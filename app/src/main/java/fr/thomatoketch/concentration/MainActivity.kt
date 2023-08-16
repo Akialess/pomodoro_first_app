1475e1977d3c3f2b3355b2595541b7e68671baa5
Akialess <alessandro.giansante03@gmail.com>
2023-08-16 18:01:17 +0200

    Add navigation bar

--- app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
+++ app/src/main/java/fr/thomatoketch/concentration/MainActivity.kt
@@ -3,7 +3,10 @@ package fr.thomatoketch.concentration
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.thomatoketch.concentration.fragments.HomeFragment
+import fr.thomatoketch.concentration.fragments.TaskFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_page -> {
+                    loadFragment(HomeFragment(this))
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.task_page -> {
+                    loadFragment(TaskFragment(this))
+                    return@setOnNavigationItemSelectedListener true
+                }
+                //TODO("modifier pour les suivants")
+                R.id.stat_page -> {
+                    loadFragment(HomeFragment(this))
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.setting_page -> {
+                    loadFragment(HomeFragment(this))
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                else -> false
+            }
+        }
+
+        loadFragment(HomeFragment(this))
+    }
+
+    private fun loadFragment(fragment: Fragment) {
         //charger notre FolderRepository
         val repo = FolderRepository()
 
@@ -20,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             //le code ici sera execute apres avoir recuperer le call back
             //injecter le fragment dans notre boite (fragment_container)
             val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, HomeFragment(this)) //modifier le dernier param pour afficher une autre page
+            transaction.replace(R.id.fragment_container, fragment) //modifier le dernier param pour afficher une autre page
             transaction.addToBackStack(null)
             transaction.commit()
             Log.d("TAG", "here 1")
